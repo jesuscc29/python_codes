ed7a6f20ad44f0e6045fbf00b040a6f1233c15d0
jesuscc29 <jesus@i-w.mx>
2015-01-12 00:17:06 -0600

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,19 @@
+import time
+import datetime
+
+
+def date_time_functions():
+  """
+  Usage of datetime functions
+  """
+  print "Time in seconds since the epoch: %s" %time.time()
+  print "Current date and time: " , datetime.datetime.now()
+  print "Or like this: " ,datetime.datetime.now().strftime("%y-%m-%d-%H-%M")
+  
+  print "Current year: ", datetime.date.today().strftime("%Y")
+  print "Month of year: ", datetime.date.today().strftime("%B")
+  print "Week number of the year: ", datetime.date.today().strftime("%W")
+  print "Weekday of the week: ", datetime.date.today().strftime("%w")
+  print "Day of year: ", datetime.date.today().strftime("%j")
+  print "Day of the month : ", datetime.date.today().strftime("%d")
+  print "Day of week: ", datetime.date.today().strftime("%A")

4ac67f7aee22845d2549ca0d86e5e5ddd9dfdc74
Jesús Cota <jesus@i-w.mx>
2015-03-06 11:43:07 -0600

    Update init.js

--- init.js
+++ init.js
@@ -46,3 +46,7 @@ var url = document.location.toString();
 if (url.match('#')) {
     $('.nav-tabs a[href=#' + url.split('#')[1] + ']').tab('show');
 }
+// Change hash for page-reload
+           $('.nav-tabs a').on('click', function (e) {
+               window.location.hash = e.target.hash;
+           });
